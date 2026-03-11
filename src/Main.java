53b78b34be05d605ef2d013ae0737453afb8fbbe
Isabelle Bright <131718387+isabright@users.noreply.github.com>
2026-03-11 10:54:33 -0500

    reads experiment file and parses line into array

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,41 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        File partFile = new File(args[0]);
+        Scanner fileReader = new Scanner(partFile);
+        ArrayList<Integer> ax = new ArrayList<>();
+        ArrayList<Integer> ay = new ArrayList<>();
+        ArrayList<Integer> bx = new ArrayList<>();
+        ArrayList<Integer> by = new ArrayList<>();
+        String[] info = new String[9];
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String data = "";
+
+        while (fileReader.hasNext()){
+            data = fileReader.nextLine();
+
+            if (data.contains("training") || data.isEmpty()){
+                continue;
+            }
+
+            info = data.trim().split("\\s+");
+            break;
+
+        } // end while
+
+        for (String word : info) {
+            System.out.println(word);
         }
+
+        System.out.println(partFile.getName());
+
     }
 }
